36ca137f7657d73f54d9d4e9c79619dbc5e0c433
Salman-Saleem001 <salman.thepearlsystem@gmail.com>
2023-10-31 06:20:18 -0700

    test3

--- lib/main.dart
+++ lib/main.dart
@@ -120,7 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.abc_sharp),
+        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
